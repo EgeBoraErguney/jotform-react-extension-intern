10ed3a817d49b222d4ed36b5bc10afe735c18b23
Ege <57564894+EgeBoraErguney@users.noreply.github.com>
2021-09-16 09:49:27 +0300

    Add Login SDK

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import "./App.css";
+import React, { useState, useEffect, useReducer } from "react";
+import axios from "axios";
+import qs from "qs";
+import { Helmet } from "react-helmet";
 
 function App() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const [userData, setUserData] = useState({});
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+  const [apiKey, setApiKey] = useState("");
+
+
+  function handleChange(event) {
+    setFormData({
+      username:
+        event.target.name === "username"
+          ? event.target.value
+          : formData.username,
+      password:
+        event.target.name === "password"
+          ? event.target.value
+          : formData.password,
+    });
+  }
+  function loginFunction() {
+    window['JF'].login(
+      function success(){
+          setApiKey(window['JF'].getAPIKey());
+          console.log(window['JF'].getAPIKey());
+      },
+      function error(){
+        window.alert("Could not authorize user");
+      }
+  ); 
+  }
+
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <button onClick={()=>loginFunction()}>Login</button>
+      {apiKey}
     </div>
   );
 }
